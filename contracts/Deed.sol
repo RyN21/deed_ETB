7160fd5eb4f4dd647adc1f271bb0defc302e300a
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-12-16 12:41:22 -0700

    Create 4 initial variables for deed smart contract

--- contracts/Deed.sol
+++ contracts/Deed.sol
@@ -1,5 +1,10 @@
 pragma solidity ^0.5.0;
 
+// legal document a parent signs infront of lawyer that states how their assets will be distributed after they pass away
 contract Deed {
-  
+  // 4 variables to a deed
+  address public lawyer;
+  address payable public beneficiary;
+  uint public amount;
+  uint public earliest;
 }
