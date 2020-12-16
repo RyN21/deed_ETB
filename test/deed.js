83caad810b3d2f921d9ca28de19f0ace70c389c0
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-12-16 13:27:00 -0700

    Create sad path test if withdraw is too early

--- test/deed.js
+++ test/deed.js
@@ -5,4 +5,23 @@ contract('Deed', (accounts) => {
   before(async () => {
     deed = await Deed.deployed();
   });
+  it('Should withdraw ether to beneficiary', async () => {
+    // Need to wait 5 seconds based on 'earliest' variable on deployed contract
+    // So we use a promise and 'setTimout'
+    const initialBalance = web3.utils.toBN(await web3.eth.getBalance(accounts[1]));
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    await deed.withdraw({from: accounts[0]});
+    const finalBalance = web3.utils.toBN(await web3.eth.getBalance(accounts[1]));
+    assert(finalBalance.sub(initialBalance).toNumber === 100);
+  });
+
+  it('Should NOT withdraw too early', async () => {
+    try {
+      await deed.withdraw({from: accounts[0]});
+    } catch(e) {
+      assert(e.message.includes('Too early');
+      return;
+    }
+    assert(false);
+  });
 });
